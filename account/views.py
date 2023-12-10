d0f46b68ae12f8a025613821fdc29957e30b007a
Reza <example email>
2023-12-10 21:19:26 +0330

    create account_register view

--- account/views.py
+++ account/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .forms import RegistrationForm
+from django.shortcuts import redirect
 
-# Create your views here.
+
+def account_register(request):
+    if request.user.is_authenticated():
+        return redirect('/')
+
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.email = form.cleaned_data['email']
+            user.set_password = form.cleaned_data['password']
+            user.is_active = False
+            user.save()
+
+
