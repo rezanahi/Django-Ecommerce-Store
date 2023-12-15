f202ad06649cf8ad0487547542541f70178ffaaf
Reza <m.rezanahi.d@gmail.com>
2023-12-15 15:15:54 +0330

    Updated registration form with validation methods

--- account/forms.py
+++ account/forms.py
@@ -11,3 +11,32 @@ class RegistrationForm(forms.ModelForm):
     class Meta:
         model = UserBase
         fields = ('user_name', 'email',)
+
+    def clean_user_name(self):
+        user_name = self.cleaned_data['user_name'].lower()
+        queryset = UserBase.objects.filter(user_name=user_name).count()
+        if queryset:
+            raise ValueError('this username already exist')
+        return user_name
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if UserBase.objects.filter(email=email).exists():
+            raise ValueError('Please use another Email, that is already taken')
+        return email
+
+    def clean_password2(self):
+        if self.cleaned_data['password'] != self.cleaned_data['password2']:
+            raise ValueError('passwords do not match')
+        return self.cleaned_data['password2']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['user_name'].widget.attrs.update(
+            {'class': 'form-control mb-3', 'placeholder': 'Username'})
+        self.fields['email'].widget.attrs.update(
+            {'class': 'form-control mb-3', 'placeholder': 'E-mail', 'name': 'email', 'id': 'id_email'})
+        self.fields['password'].widget.attrs.update(
+            {'class': 'form-control mb-3', 'placeholder': 'Password'})
+        self.fields['password2'].widget.attrs.update(
+            {'class': 'form-control', 'placeholder': 'Repeat Password'})
