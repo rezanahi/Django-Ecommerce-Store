05b18888ef076e754bf53e54efb3d0a861975af6
Reza <m.rezanahi.d@gmail.com>
2023-12-18 19:42:03 +0330

    complete password reset functions

--- account/urls.py
+++ account/urls.py
@@ -24,6 +24,10 @@ urlpatterns = [
                                                                                       success_url='/account/password_reset_complete/',
                                                                                       form_class=PwdResetConfirmForm),
          name='password_reset_confirm'),
+    path('password_reset/password_reset_email_confirm/', TemplateView.as_view(template_name='account/user/reset_status.html'),
+         name='password_reset_done'),
+    path('password_reset_complete/', TemplateView.as_view(template_name='account/user/reset_status.html'),
+         name='password_reset_complete')
 
 ]
 
