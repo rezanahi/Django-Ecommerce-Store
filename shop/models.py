7198bb37a2e470daeacf433b013edc7bbbbe360d
Reza <m.rezanahi.d@gmail.com>
2023-12-31 21:11:29 +0330

    add Category MPTTModel

--- shop/models.py
+++ shop/models.py
@@ -3,5 +3,25 @@ from django.urls import reverse
 from mptt.models import MPTTModel, TreeForeignKey
 
 
+class Category(MPTTModel):
+    name = models.CharField(max_length=255, unique=True, verbose_name='Category name', help_text='Required and uniq')
+    slug = models.SlugField(max_length=255, unique=True, verbose_name='Category safe url')
+    parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+    is_active = models.BooleanField(default=True)
+
+    class MPTTMeta:
+        order_insertion_by = ['name']
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def get_absolute_url(self):
+        return reverse(viewname='shop:category_list', args=[self.slug])
+
+    def __str__(self):
+        return self.name
+
+
 
 
